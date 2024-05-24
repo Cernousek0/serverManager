75593b3eb670235e7f53851fe0975222cc7d0804
jirkos.eu <146726029+jirkoseu@users.noreply.github.com>
2024-05-24 11:16:16 +0200

    curarafsasfaf

--- app/frontend/tailwind.config.js
+++ app/frontend/tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-// tailwind.config.js
 module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily: {
-        'dongle': ['Dongle', 'sans-serif'],
-        'fira-mono': ['FiraMono', 'monospace'],
-        'doppio-one': ['DoppioOne', 'sans-serif'],
-      },
-    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
-}
-
-
+};
