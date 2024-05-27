5b7bbcf7a84b1403f7fc4c7f3af2db0c20ea8b3c
jirkos.eu <146726029+jirkoseu@users.noreply.github.com>
2024-05-27 08:44:43 +0200

    shdkjkhjfds

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -9,6 +9,23 @@ function App() {
   const [setupServer, setSetup] = useState(false);
 
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState("Select game");
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const handleOptionClick = (option) => {
+    setSelected(option);
+    setIsOpen(false);
+  };
+
+  const ServerTypeOptions = [
+    "Minecraft","Terraria","Counter Strike 1.6"
+  ];
+  const ServerVersionTypeOptions = [
+    "Forge","Vanilla","Spigot"
+  ];
+
   return (
     <div className="flex h-screen w-full">
       <div className='w-full flex-col'>
@@ -16,12 +33,60 @@ function App() {
         <h1 className="text-2xl p-2 fdp flex-row flex gap-2">ServerSphere<img className='w-7' src='./serversphere_logo.png'></img></h1>
       </div>
       <div className="text-red h-5/6 w-full flex items-center justify-center">
-        <div className={`${setupServer ? "h-5/6" : "h-48"} flex flex-col items-center justify-center bg-[#ECECEC] border-[#BFBFBF] border w-2/3 rounded-xl text-[#BFBFBF] hover:text-[#9C9C9C] duration-300 ease-in-out cursor-pointer`} onClick={() => setSetup(true)}>
+        <div className={`${setupServer ? "h-5/6 justify-start" : "h-48 justify-center"} flex gap-2 flex-col items-center bg-[#ECECEC] border-[#BFBFBF] border w-2/3 rounded-xl text-[#BFBFBF] hover:text-[#9C9C9C] duration-300 ease-in-out`} onClick={() => setSetup(true)}>
+        {/* Setup server */}
         {setupServer ? (
         <> 
-          <p className='fm'>SETUP SERVER</p>
-          <p className='fm text-4xl'>+</p>
-          <input type='text'></input>
+          <div className="relative inline-block w-3/4 mt-2">
+      <button onClick={toggleDropdown} className={`${isOpen ? "rounded-t-lg":"rounded-xl"} flex flex-row items-center justify-start w-3/4 h-8 p-3 border-[#DCDCDC] bg-white overflow-elippsis border hover:bg-gray-200  hover:border-gray-300 duration-150 ease-in-out`}>
+        <span className="mr-1">{selected}</span>
+        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+        </svg>
+      </button>
+      {isOpen && (
+        <ul className="absolute z-10 flex flex-col items-center justify-center w-3/4 gap-1 p-1 bg-white overflow-elippsis border border-[#DCDCDC] duration-150 ease-in-out rounded-b-lg">
+          {ServerTypeOptions.map(option => (
+            <li key={option} onClick={() => handleOptionClick(option)} className="cursor-pointer flex flex-row items-center justify-start w-full p-1 ps-2 bg-white rounded-lg overflow-elippsis hover:bg-gray-200  duration-150 ease-in-out">
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+      {selected === "Minecraft" && (
+        <p></p>
+      )}
+      {selected === "Terraria" && (
+        <div className="mt-4 p-4 text-black bg-orange-100 border border-orange-500 rounded-md">
+          Comming soon...
+        </div>
+      )}
+    </div>
+    <div className="relative inline-block w-3/4">
+      <button onClick={toggleDropdown} className={`${isOpen ? "rounded-t-lg":"rounded-xl"} flex flex-row items-center justify-start w-3/4 h-8 p-3 border-[#DCDCDC] bg-white overflow-elippsis border hover:bg-gray-200  hover:border-gray-300 duration-150 ease-in-out`}>
+        <span className="mr-1">{selected}</span>
+        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+        </svg>
+      </button>
+      {isOpen && (
+        <ul className="absolute z-10 flex flex-col items-center justify-center w-3/4 gap-1 p-1 bg-white overflow-elippsis border border-[#DCDCDC] duration-150 ease-in-out rounded-b-lg">
+          {ServerVersionTypeOptions.map(option => (
+            <li key={option} onClick={() => handleOptionClick(option)} className="cursor-pointer flex flex-row items-center justify-start w-full p-1 ps-2 bg-white rounded-lg overflow-elippsis hover:bg-gray-200  duration-150 ease-in-out">
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+      {selected === "Minecraft" && (
+        <p></p>
+      )}
+      {selected === "Terraria" && (
+        <div className="mt-4 p-4 text-black bg-orange-100 border border-orange-500 rounded-md">
+          Comming soon...
+        </div>
+      )}
+    </div>
         </>
         ) : (
         <>
@@ -33,7 +98,8 @@ function App() {
         </div>
       </div>
       </div>
-      <div className="bg-[#ECECEC] border-[#DCDCDC] items-center border-s text-red h-full w-64 flex flex-col justify-start">
+      {/* Server list */}
+      <div className="bg-[#ECECEC] border-[#DCDCDC] items-center border-s text-red h-full w-64 flex flex-col justify-start hover:w-72 duration-300 ease-in-out">
         <p className='fdp self-start m-1 text-xl ms-2'>My servers</p>
         <div className='flex flex-col items-center justify-center w-full max-h-11/12 gap-2 overflow-auto'>
           <div className='flex flex-col items-center justify-start w-10/12 h-12 bg-white rounded-xl overflow-elippsis border border-white hover:bg-gray-200  hover:border-gray-300 duration-150 ease-in-out cursor-pointer'>
